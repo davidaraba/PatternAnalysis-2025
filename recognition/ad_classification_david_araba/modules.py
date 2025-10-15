7e1aafb89fa6d9e1c580fd48c7240c18e0281192
David Araba <daviidaraba@outlook.com>
2025-10-15 10:05:09 +1000

    feat: implement main convnext model

--- recognition/ad_classification_david_araba/modules.py
+++ recognition/ad_classification_david_araba/modules.py
@@ -68,3 +68,66 @@ class Block(nn.Module):
 
         x = input_tensor + self.drop_path(x)
         return x
+
+# --- Main ConvNeXt Model --- 
+class ConvNeXt(nn.Module):
+    """
+    ConvNeXt model built from scratch.
+    Paper: `A ConvNet for the 2020s` (https://arxiv.org/pdf/2201.03545.pdf)
+    """
+    def __init__(self, in_chans=1, num_classes=2, 
+                depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], 
+                drop_path_rate=0., layer_scale_init_value=1e-6, 
+                head_init_scale=1.):
+        super().__init__()
+
+        # Stem and downsampling layers
+        self.downsample_layers = nn.ModuleList()
+        stem = nn.Sequential(
+            nn.Conv2d(in_chans, dims[0], kernel_size=4, stride=4),
+            LayerNorm(dims[0], eps=1e-6, data_format="channels_first")
+        )
+        self.downsample_layers.append(stem)
+
+        for i in range(3):
+            downsample_layer = nn.Sequential(
+                LayerNorm(dims[i], eps=1e-6, data_format="channels_first"),
+                nn.Conv2d(dims[i], dims[i+1], kernel_size=2, stride=2),
+            )
+            self.downsample_layers.append(downsample_layer)
+
+        # Main stages composed of ConvNeXt blocks
+        self.stages = nn.ModuleList()
+        dp_rates = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))] 
+        cur = 0
+        for i in range(4):
+            stage = nn.Sequential(
+                *[Block(dim=dims[i], drop_path=dp_rates[cur + j], 
+                layer_scale_init_value=layer_scale_init_value) for j in range(depths[i])]
+            )
+            self.stages.append(stage)
+            cur += depths[i]
+
+        self.norm = nn.LayerNorm(dims[-1], eps=1e-6) # final norm layer
+        self.head = nn.Linear(dims[-1], num_classes)
+
+        self.apply(self._init_weights)
+        self.head.weight.data.mul_(head_init_scale)
+        self.head.bias.data.mul_(head_init_scale)
+
+    def _init_weights(self, m):
+        if isinstance(m, (nn.Conv2d, nn.Linear)):
+            trunc_normal_(m.weight, std=.02)
+            nn.init.constant_(m.bias, 0)
+
+    def forward_features(self, x):
+        for i in range(4):
+            x = self.downsample_layers[i](x)
+            x = self.stages[i](x)
+        # Global average pooling
+        return self.norm(x.mean([-2, -1]))
+
+    def forward(self, x):
+        x = self.forward_features(x)
+        x = self.head(x)
+        return x
